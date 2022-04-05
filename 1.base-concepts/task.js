3d0610174cfe25753277e8a5b67bd84295319b42
elena-gireva <94042128+elena-gireva@users.noreply.github.com>
2022-04-05 11:38:25 +0300

    add function solveEquation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,27 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant;
+  discriminant = b**2-4*a*c;
+
+  if (a===0){
+    alert(`Коэффициент "a" не может быть равным нулю!`); 
+  }
+  
+  else if (discriminant < 0) {
+    alert("Дискриминант = " + discriminant + " Корней нет!"); 
+  } 
+  else if (discriminant === 0) {
+    let x1 = (-b/2*a);
+    arr.push(x1);
+  } 
+  else {
+    let x1 = (-b + Math.sqrt(discriminant) )/(2*a);
+    let x2 = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(x1,x2);
+  };
+
   return arr; // array
 }
 
