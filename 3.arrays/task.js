7f254950a5a5a220e23ae4f21d979dfe5e1cd87b
elena-gireva <94042128+elena-gireva@users.noreply.github.com>
2022-04-14 16:40:46 +0300

    add task3 Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = false;
+  
+  if (arr1.length === arr2.length){
+    
+    result = arr1.every(item => arr2[arr1.indexOf(item)] === item);
+  
+  }
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map(item => item*10)
   return resultArr; // array
 }
